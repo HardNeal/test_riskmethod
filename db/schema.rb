0d711c34689636d4f93f5b90fb1c17906bbc5e9d
Primov Ulan <ulan.primov@gmail.com>
2019-02-22 15:05:12 +0100

    added rating model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_22_133251) do
+ActiveRecord::Schema.define(version: 2019_02_22_135640) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "category_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_movies_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
