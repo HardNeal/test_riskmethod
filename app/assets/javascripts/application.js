a5fc55148bed82f1883096b63e841e723be78f8b
Primov Ulan <ulan.primov@gmail.com>
2019-02-22 17:44:40 +0100

    added devise views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
-//= require rails-ujs
 //= require jquery3
+//= require rails-ujs
 //= require bootstrap-sprockets
+//= require_tree .
+//= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_22_135640) do
+ActiveRecord::Schema.define(version: 2019_02_22_140238) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2019_02_22_135640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_movies_on_category_id"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "movie_id"
+    t.integer "star"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
